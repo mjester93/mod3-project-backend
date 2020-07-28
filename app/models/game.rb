4e9c0dd55d1df3d53509e0592d5e7159895d37c4
mjester93 <m.jester.93@gmail.com>
2020-07-28 14:28:47 -0400

    game

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,7 +3,7 @@ class Game < ApplicationRecord
     has_many :reviews
 
     def ordered_reviews
-        self.class.select('reviews.id, reviews.score, reviews.summary, users.id, users.name, reviews.created_at').
+        self.class.select('reviews.id, reviews.score, reviews.summary, users.id AS user_id, users.name AS user_name, reviews.created_at').
         joins("LEFT JOIN reviews ON reviews.game_id = #{self.id} LEFT JOIN users ON users.id = reviews.user_id").
         where('games.id' => self.id)
     end
