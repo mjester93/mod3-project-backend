8f8470553772abfc42af42d12a1ac2759fb24f76
mjester93 <m.jester.93@gmail.com>
2020-07-29 09:28:17 -0400

    user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,10 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
+    def show
+        user = User.find(params[:id])
+        render json: user.to_json()
+    end
+
+    def create
+        User.create(name: params[:name])
+    end
 end
