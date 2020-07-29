ddcb5d728f7442aec958ded974dca30aef92baeb
mjester93 <m.jester.93@gmail.com>
2020-07-29 14:18:56 -0400

    wishlist controller

--- app/controllers/api/v1/user_games_controller.rb
+++ app/controllers/api/v1/user_games_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::UserGamesController < ApplicationController
+    def create
+        user_game = UserGame.create(game_id: params[:game_id], user_id: params[:user_id])
+        render json: user_game.to_json(
+            include: [:game],
+            exclude: [:updated_at, :created_at]
+        )
+    end
 end
--- app/controllers/api/v1/user_wishlists_controller.rb
+++ app/controllers/api/v1/user_wishlists_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::UserWishlistsController < ApplicationController
+    def index
+        user_wishlist = UserWishlist.first
+        render json: user_wishlist.to_json(
+            include: [:game],
+            exclude: [:updated_at, :created_at]
+        )
+    end
+
+    def create
+        user_wishlist = UserWishlist.create(game_id: params[:game_id], user_id: params[:user_id])
+        render json: user_wishlist.to_json(
+            include: [:game],
+            exclude: [:updated_at, :created_at]
+        )
+    end
 end
